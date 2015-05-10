734b5d80e799b84e52c194aed1b7eb00690f2e42
Vasile Popescu <elisescu@elisescu.com>
2015-05-10 23:11:22 +0200

    link the rtaudio in the current small test app

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <RtAudio.h>
 
 int main(int argc, char *argv[])
 {
+    RtAudio audio;
+    RtAudio::DeviceInfo info;
+
+    unsigned int devices = audio.getDeviceCount();
+    std::cout << "\nFound " << devices << " device(s) ...\n";
     std::cout << "Hi there\n";
     return 1;
 }
